7feb274f4f33e65cba5dfffc97d07b7c61c7502b
giapminh146 <giapdoanhminh@gmail.com>
2024-09-02 19:17:09 +0700

    Finished Practical 4

--- USTH Weather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTH Weather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -1,18 +1,42 @@
 package vn.edu.usth.weather;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ForecastFragment extends Fragment {
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(0x20FF0000);
-        return view;
+//        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+//        view.setBackgroundColor(0x20FF0000);
+//        return view;
+        LinearLayout linearLayout = new LinearLayout(getActivity());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setPadding(20, 20, 20, 20);
+        linearLayout.setGravity(Gravity.CENTER);
+
+        TextView textView = new TextView(getActivity());
+        textView.setText("Thursday");
+        textView.setTextSize(30);
+        textView.setPadding(0, 0, 0, 20);
+
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setImageResource(R.drawable.cloudy);
+
+        linearLayout.addView(textView);
+        linearLayout.addView(imageView);
+
+        return linearLayout;
     }
 }
